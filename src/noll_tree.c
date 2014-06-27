8770a36d36c954ff9b73f45fc2fbd2dfaae00ecf
Ondra Lengal <ondra.lengal@gmail.com>
2014-06-27 22:49:13 +0200

    noll_tree: add noll_tree_to_ta()

--- src/noll_tree.c
+++ src/noll_tree.c
@@ -21,41 +21,10 @@
 /*                                                                        */
 /**************************************************************************/
 
-#ifndef _NOLL_TREE_H_
-#define _NOLL_TREE_H_
+#include "noll_tree.h"
 
-#include "noll_ta_symbols.h"
-#include "libvata_noll_iface.h"
-
-struct noll_tree_node_t;
-
-NOLL_VECTOR_DECLARE (noll_tree_node_array, struct noll_tree_node_t*);
-
-/**
- * @brief  A tree node
- */
-typedef struct noll_tree_node_s
+noll_ta_t* noll_tree_to_ta(noll_ta_t* tree)
 {
-	/// The symbol in the node
-	const noll_ta_symbol_t* symbol;
-
-	/// Children of the node (ordered)
-	noll_tree_node_array* children;
-} noll_tree_node_t;
-
-
-/// The tree - now an alias for the root node
-typedef noll_tree_node_t noll_tree_t;
-
-/**
- * @brief  Converts a tree into a tree automaton
- *
- * The tree automaton accepts exactly the input tree
- *
- * @param[in]  tree  The tree to be converted into a tree automaton
- *
- * A tree automaton accepting exactly @p tree
- */
-noll_ta_t* noll_tree_to_ta(noll_ta_t* tree);
-
-#endif /* _NOLL_TREE_H_ */
+	assert(NULL != tree);
+	return tree;
+}
